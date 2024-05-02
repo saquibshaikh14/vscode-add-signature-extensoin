6bdc6b2986c4b0931299c24932f6f30d9ccce663
Saquib Shaikh <saquibmd82@gmail.com>
2024-05-02 18:39:07 +0530

    Updated Comment input and Comment generation

--- README.md
+++ README.md
@@ -1,27 +1,45 @@
 # Vscode Add Signature Extension
 
 ## Description
+
 The Vscode Add Signature extension automatically adds a preloaded signature to the top of each file whenever it is created. Users can configure the signature text through the extension settings.
 
 ## Activation
+
 The extension is activated when Visual Studio Code starts. It listens for the creation of new files and triggers the addition of the signature text.
 
 ## Configuration
-* Open vscode **Command Pallet** by pressing cmd + shift + p in mac or cntrl + shift + p in window.
-* Search `VscodeAddSignature: Configure` and open.
+
+-   Open vscode **Command Pallet** by pressing cmd + shift + p in mac or cntrl + shift + p in window.
+-   Search `VscodeAddSignature: Configure` and open extension setting.
 
 Configure the extension through the following settings:
-- **Enable Add Signature**: Toggle to enable/disable adding the signature text to new files.
-- **Signature Text**: Enter the signature text to be added at the top of each new file. Use `\n` for line breaks.
+
+-   **Enable Add Signature**: Toggle to enable/disable adding the signature text to new files.
+-   **Signature Text**: Enter the signature text to be added at the top of each new file. For new line in comment use ```Enter```.
+
+**Note:** To use new line without ```*``` press ```Enter``` and start typing. See **Example 2**
+
+### Supported file types
+```.js``` ```.ts``` ```.rb``` ```.rs``` ```.php``` ```.java``` ```.kt``` ```.kts``` ```.c``` ```.html``` ```.css```
 
 ### Signature Text Example
 
 Here's an example of how to format the signature text:
+
+#### Example with *
+
+**Input**
 ```javaScript
-/**\n* author Saquib Shaikh\n* created on ${DD-MM-YYYY-hh-mm}\n* github: https://github.com/saquibshaikh14\n* copyright ${YEAR}\n**/
+ * author Saquib Shaikh
+ * created on 02-05-2024-17h-48m
+ * github: https://github.com/saquibshaikh14
+ * copyright 2024
 ```
 
-File output
+**File output**
+
+***javascript & typescript***
 ```javaScript
 /**
 * author Saquib Shaikh
@@ -30,24 +48,62 @@ File output
 * copyright 2024
 **/
 ```
+***html***
+```html
+<!--
+ * author Saquib Shaikh
+ * created on 02-05-2024-17h-50m
+ * github: https://github.com/saquibshaikh14
+ * copyright 2024
+-->
+```
+
+#### Example without \*
+**Input**
+```javaScript
+ author Saquib Shaikh
+ created on 02-05-2024-17h-48m
+ github: https://github.com/saquibshaikh14
+ copyright 2024
+```
+***javascript & typescript***
+```javaScript
+/**
+ author Saquib Shaikh
+ created on 16-03-2024-10-30
+ github: https://github.com/saquibshaikh14
+ copyright 2024
+**/
+```
+***html***
+```html
+<!--
+ author Saquib Shaikh
+ created on 02-05-2024-17h-50m
+ github: https://github.com/saquibshaikh14
+ copyright 2024
+-->
+```
 
 ## Contributing and Bug Reporting
+
 To contribute to the development of this extension or report any issues, visit the [GitHub repository](https://github.com/saquibshaikh14/vscode-add-signature-extensoin.git). Feel free to raise issues, submit pull requests, or contribute in any way you see fit.
 
-## Known Issues
-Untitled notes saving causes issues. If a file is saved and then shown as unsaved without modification, closing the file may result in the loss of changes.
 
 ## File Handling
+
 The extension handles the following file-related operations:
 
-- **onDidCreateFiles**: Listens for the creation of new files and adds the signature text to them.
-- **addSignatureToFile**: Adds the configured signature text to the top of each new file.
+-   **onDidCreateFiles**: Listens for the creation of new files and adds the signature text to them.
+-   **addSignatureToFile**: Adds the configured signature text to the top of each new file.
 
 ## Deactivation
+
 The extension's deactivate function is called when the extension is deactivated. However, since the extension primarily operates based on events triggered by file creation, there is no specific deactivation logic implemented.
 
 ## Author
-- **Name**: Saquib Shaikh
-- **Email**: mdsqb0786@gmail.com
+
+-   **Name**: Saquib Shaikh
+-   **Email**: mdsqb0786@gmail.com
 
 Feel free to modify or enhance this extension as needed. Your contributions are welcome!
--- src/extension.ts
+++ src/extension.ts
@@ -6,12 +6,24 @@ import constans from "./constans";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	let onCredteFilesDisposable = vscode.workspace.onDidCreateFiles(
+	let onCreateFilesDisposable = vscode.workspace.onDidCreateFiles(
 		(event: vscode.FileCreateEvent) => {
 			event.files.forEach((uri) => {
+				console.log(uri);
+				// eslint-disable-next-line eqeqeq
 				if (uri.scheme == "file") {
 					//do stuff
-					addSignatureToFile(uri);
+					// addSignatureToFile(uri);
+					vscode.workspace.fs.stat(uri).then((stats) => {
+						if (stats.type === vscode.FileType.File) {
+							addSignatureToFile(uri);
+							//add signature if it is file.
+						}
+						/*else if (stats.type === vscode.FileType.Directory) {
+							// It's a directory, you might want to handle this case differently
+							console.log("A directory was created:", uri);
+						}*/
+					});
 				}
 			});
 		}
@@ -28,7 +40,7 @@ export function activate(context: vscode.ExtensionContext) {
 	);
 
 	context.subscriptions.push(
-		onCredteFilesDisposable,
+		onCreateFilesDisposable,
 		commandConfigurationDisposable
 	);
 
@@ -45,8 +57,16 @@ async function addSignatureToFile(uri: vscode.Uri) {
 	let enableAddSignature = config.get(constans.ENABLE_ADD_SIGNATURE) || false;
 	let signatureText = config.get<string>(constans.SIGNATURE_TEXT) || "";
 
+	const fileExtension = uri.fsPath
+		.toLowerCase()
+		.slice(uri.fsPath.lastIndexOf("."));
 	let parsedSignatureText = replacePattern(signatureText);
-
+	// console.log(parsedSignatureText);
+	const comment = generateComment(fileExtension, parsedSignatureText);
+	if (!comment) {
+		//if file extension is not supported dont add anything
+		return;
+	}
 	if (!enableAddSignature) {
 		return;
 	}
@@ -56,11 +76,7 @@ async function addSignatureToFile(uri: vscode.Uri) {
 	try {
 		const document = await vscode.workspace.openTextDocument(uri);
 		const edit = new vscode.WorkspaceEdit();
-		edit.insert(
-			uri,
-			new vscode.Position(0, 0),
-			parsedSignatureText + "\n\n"
-		);
+		edit.insert(uri, new vscode.Position(0, 0), comment);
 
 		await vscode.workspace.applyEdit(edit);
 		await document.save();
@@ -85,41 +101,97 @@ async function delay(ms: number) {
 }
 
 function replacePattern(text: string): string {
-    const now = new Date();
-    const year = now.getFullYear().toString();
-    const month = (now.getMonth() + 1).toString().padStart(2, "0");
-    const day = now.getDate().toString().padStart(2, "0");
-    const hours = now.getHours().toString().padStart(2, "0");
-    const minutes = now.getMinutes().toString().padStart(2, "0");
+	const now = new Date();
+	const year = now.getFullYear().toString();
+	const month = (now.getMonth() + 1).toString().padStart(2, "0");
+	const day = now.getDate().toString().padStart(2, "0");
+	const hours = now.getHours().toString().padStart(2, "0");
+	const minutes = now.getMinutes().toString().padStart(2, "0");
 
-    // Preserve new line
-    text = text.replace(/\\n/g, "\n");
+	// Preserve new line
+	// text = text.replace(/\\n/g, "\n");
 
-    // Replace each component of the date pattern with the corresponding value
-    // Use a regular expression with capturing groups to match any order of date components
-    return text.replace(
-        /\${(DD|MM|YYYY|hh|mm|YEAR)-?(DD|MM|YYYY|hh|mm|YEAR)?-?(DD|MM|YYYY|hh|mm|YEAR)?-?(DD|MM|YYYY|hh|mm|YEAR)?-?(DD|MM|YYYY|hh|mm|YEAR)?}/g,
-        (match, p1, p2, p3, p4, p5) => {
-            const components = [p1, p2, p3, p4, p5].filter(Boolean); // Remove undefined components
-            const orderedValues = components.map((component) => {
-                switch (component) {
-                    case "DD":
-                        return day;
-                    case "MM":
-                        return month;
-                    case "YYYY":
-                        return year;
-                    case "hh":
-                        return hours + "h";
-                    case "mm":
-                        return minutes + "m";
-                    case "YEAR":
-                        return year;
-                    default:
-                        return component; // Leave unknown components unchanged
-                }
-            });
-            return orderedValues.join("-"); // Join the ordered values with '-'
-        }
-    );
+	// Replace each component of the date pattern with the corresponding value
+	// Use a regular expression with capturing groups to match any order of date components
+	return text.replace(
+		/\${(DD|MM|YYYY|hh|mm|YEAR)-?(DD|MM|YYYY|hh|mm|YEAR)?-?(DD|MM|YYYY|hh|mm|YEAR)?-?(DD|MM|YYYY|hh|mm|YEAR)?-?(DD|MM|YYYY|hh|mm|YEAR)?}/g,
+		(match, p1, p2, p3, p4, p5) => {
+			const components = [p1, p2, p3, p4, p5].filter(Boolean); // Remove undefined components
+			const orderedValues = components.map((component) => {
+				switch (component) {
+					case "DD":
+						return day;
+					case "MM":
+						return month;
+					case "YYYY":
+						return year;
+					case "hh":
+						return hours + "h";
+					case "mm":
+						return minutes + "m";
+					case "YEAR":
+						return year;
+					default:
+						return component; // Leave unknown components unchanged
+				}
+			});
+			return orderedValues.join("-"); // Join the ordered values with '-'
+		}
+	);
+}
+
+function generateComment(fileExtension: string, parsedSignatureText: string) {
+	console.log({ fileExtension, parsedSignatureText });
+
+	// Define a mapping of file extensions to their respective comment block formats
+	//supported files.
+	const commentFormats: { [key: string]: string } = {
+		".js": "/**\n",
+		".ts": "/**\n",
+		".html": "<!--\n",
+		".css": "/*\n",
+		".rb": "=begin\n",
+		".rs": "/*\n",
+		".java": "/*\n",
+		".kt": "/*\n",
+		".kts": "/*\n",
+		".php": "/*\n",
+		".c": "/*\n",
+		
+		// Add more file extensions and their corresponding comment block formats as needed
+	};
+
+	// Get the comment block format for the given file extension
+	const commentFormat = commentFormats[fileExtension.toLowerCase()];
+
+	if (!commentFormat) {
+		return;
+	}
+
+	// Split the parsed signature text into an array of lines
+	let commentLines = parsedSignatureText.split("\n");
+
+	// Remove the first empty string if present
+	if (commentLines[0] === "") {
+		commentLines.shift();
+	}
+
+	let commentBlock = "";
+
+	// Generate the comment block using the comment format and parsed signature text
+	commentBlock = `${commentFormat}`;
+	commentLines.forEach((line) => {
+		commentBlock += ` ${line}\n`;
+	});
+
+	commentBlock += `${
+		commentFormat.startsWith("<!--")
+			? "-->"
+			: commentFormat.startsWith("=begin")
+			? "=end"
+			: "*/"
+	}\n\n`;
+
+	console.log(commentBlock);
+	return commentBlock;
 }
